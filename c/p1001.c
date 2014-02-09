402b97152e713f165ca4d7eefa414c543b380d16
Ji Zhang <jizhang@anjuke.com>
2014-02-09 21:23:22 +0800

    use int array, still not accepted, and I'm gonna give up

--- c/p1001.c
+++ c/p1001.c
@@ -1,65 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-void add(char result[100], char tmp[100]) {
-    int i, p, t, e1 = 0, e2 = 0;
-    p = 0;
-    for (i = 0; i < 100; ++i) {
-        t = 0;
-        if (e1 == 1 || result[i] == '\0') {
-            e1 = 1;
-        } else {
-            t += result[i] - '0';
-        }
-        if (e2 == 1 || tmp[i] == '\0') {
-            e2 = 1;
-        } else {
-            t += tmp[i] - '0';
-        }
-        t += p;
-        p = 0;
-        if (t > 9) {
-            result[i] = '0' + (t % 10);
-            p = 1;
-        } else {
-            result[i] = '0' + t;
-        }
-    }
-    for (i = 99; i > -1; --i) {
-        if (result[i] != '0') {
-            result[i + 1] = '\0';
-            break;
+void add(int result[100], int tmp[100]) {
+    int i;
+
+    for (i = 0; i < 100 - 1; ++i) {
+        result[i] += tmp[i];
+        if (result[i] > 9) {
+            result[i] -= 10;
+            result[i + 1] += 1;
         }
     }
 }
 
-void multiply(char result[100], char initial[7]) {
-    char ori[100], tmp[100];
-    int i, j, p, t;
+void multiply(int result[100], int initial[6]) {
+    int ori[100], tmp[100], i, j, p;
 
-    strcpy(ori, result);
-    strcpy(result, "0");
+    memcpy(ori, result, 100 * sizeof(int));
+    memset(result, 0, 100 * sizeof(int));
 
-    for (i = 0; i < strlen(initial); ++i) {
+    for (i = 0; i < 6; ++i) {
 
-        for (j = 0; j < i; ++j) {
-            tmp[j] = '0';
-        }
-
+        memset(tmp, 0, 100 * sizeof(int));
         p = 0;
-        for (j = 0; j < strlen(ori); ++j) {
-            t = (ori[j] - '0') * (initial[i] - '0') + p;
-            tmp[j + i] = '0' + (t % 10);
-            p = t / 10;
+        for (j = 0; j < 100 - i; ++j) {
+            tmp[j + i] = ori[j] * initial[i] + p;
+            p = 0;
+            if (tmp[j + i] > 9) {
+                p = tmp[j + i] / 10;
+                tmp[j + i] %= 10;
+            }
         }
 
-        if (p > 0) {
-            tmp[j + i] = '0' + p;
-            ++j;
-        }
-        tmp[j + i] = '\0'; 
         add(result, tmp);
     }
+
 }
 
 void revert(char *s) {
@@ -73,73 +48,59 @@ void revert(char *s) {
     }
 }
 
-void calc(char result[100], char s[7], int n) {
-    char initial[7];
-    int i, j = 0, f = 0, len;
+void calc(char r[100], char s[7], int n) {
+    int result[100], initial[6], i, j, f, flag;
 
     if (n == 0) {
-        strcpy(result, "1");
+        strcpy(r, "1");
         return;
     }
 
-    for (i = 0; i < 6; ++i) {
+    memset(result, 0, 100 * sizeof(int));
+    memset(initial, 0, 6 * sizeof(int));
+
+    j = 0;
+    f = 0;
+    for (i = 5; i > -1; --i) {
         if (s[i] == '.') {
             f = (5 - i) * n;
         } else {
-            result[j] = s[i];
-            initial[j] = s[i];
+            result[j] = s[i] - '0';
+            initial[j] = result[j];
             ++j;
         }
     }
-    result[j] = '\0';
-    initial[j] = '\0';
-
-    revert(result);
-    revert(initial);
 
     for (i = 0; i < n - 1; ++i) {
         multiply(result, initial);
     }
 
-    len = strlen(result);
-    for (i = len - 1; i > -1; --i) {
-        if (result[i] != '0') {
-            result[i + 1] = '\0';
+    j = 0;
+    flag = 0;
+    for (i = 0; i < f; ++i) {
+        if (flag || result[i] > 0) {
+            r[j++] = '0' + result[i];
+            flag = 1;
+        }
+    }
+    if (j > 0) {
+        r[j++] = '.';
+    }
+    for (i = f; i < 100; ++i) {
+        r[j++] = '0' + result[i];
+    }
+
+    for (i = 99; i > -1; --i) {
+        if (r[i] != '0') {
+            r[i + 1] = '\0';
             break;
         }
     }
     if (i == -1) {
-        strcpy(result, "0");
-        return;
+        r[1] = '\0';
     }
 
-    len = strlen(result);
-    if (f >= len) {
-        for (i = len; i < f; ++i) {
-            result[i] = '0';
-        }
-        result[i] = '.';
-        result[i + 1] = '\0';
-    } else {
-        for (i = len; i > f - 1; --i) {
-            result[i + 1] = result[i];
-        }
-        result[f] = '.';
-    }
-
-    for (i = 0; i < f; ++i) {
-        if (result[i] != '0') {
-            break;
-        }
-    }
-    if (i == f) {
-        ++i;
-    }
-    for (j = 0; j < 99; ++j) {
-        result[j] = result[j + i];
-    }
-
-    revert(result);
+    revert(r);
 }
 
 int main() {
