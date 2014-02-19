f957c38126b937a7df4c8e4f8f4fc5a07e921191
Ji Zhang <jizhang@anjuke.com>
2014-02-19 10:37:23 +0800

    time limit, of course...

--- c/p1012.c
+++ c/p1012.c
@@ -3,16 +3,39 @@
 int ks[30];
 
 int valid(int k, int m) {
-    int i, j;
+    int i, j, killed;
     
     for (i = 0; i < k; ++i) {
         ks[i] = 1;
         ks[i + k] = 2;
     }
     
-    // TODO
-
-    return 1;
+    i = m % (2 * k) - 1;
+    killed = 0;
+    while (1) {
+        
+        if (ks[i] == 1) {
+            return 0;
+        } else if (ks[i] == 2) {
+            ks[i] = 0;
+            if (++killed == k) {
+                return 1;
+            }
+        }
+        
+        j = m;
+        while (1) {
+        
+            if (++i == 2 * k) {
+                i = 0;
+            }
+        
+            if (ks[i] != 0 && --j == 0) {
+                break;
+            }
+        }
+        
+    }
 }
 
 int calc(int k) {
