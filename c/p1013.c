8686b16ebce10456f18fc794f0a4df407c63451e
Ji Zhang <jizhang@anjuke.com>
2014-02-21 23:00:42 +0800

    read input

--- c/p1013.c
+++ c/p1013.c
@@ -2,6 +2,18 @@
 
 int main() {
 
+    int n, i, j;
+    char data[9][13];
+    scanf("%d", &n);
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < 9; ++j) {
+            scanf("%s", data[j]);
+        }
+    }
+
+    for (j = 0; j < 9; ++j) {
+        printf("%s", data[j]);
+    }
 
     return 0;
 }
