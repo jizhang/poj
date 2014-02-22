696674ce01e7d85bd9ea0c206ed345f45be4b8c7
Ji Zhang <jizhang@anjuke.com>
2014-02-22 21:49:21 +0800

    calc

--- c/p1013.c
+++ c/p1013.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+
+void calc(char data[9][13], char result[2][6]) {
+    strcpy(result[0], "K");
+    strcpy(result[1], "light");
+}
 
 int main() {
 
     int n, i, j;
-    char data[9][13];
+    char data[9][13], result[2][6];
+
     scanf("%d", &n);
     for (i = 0; i < n; ++i) {
         for (j = 0; j < 9; ++j) {
             scanf("%s", data[j]);
         }
-    }
-
-    for (j = 0; j < 9; ++j) {
-        printf("%s", data[j]);
+        calc(data, result);
+        printf("%s is the counterfeit coin and it is %s.\n", result[0], result[1]);
     }
 
     return 0;
